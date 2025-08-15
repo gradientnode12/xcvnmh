d4382a0f81379c79ff8ca1bb3e864de759d4595c
gradientnode12 <queen.work.6688@gmail.com>
2025-08-15 20:58:20 +0700

    Update gcp-25project.sh

--- gcp-25project.sh
+++ gcp-25project.sh
@@ -53,8 +53,10 @@ for ((i = 0; i < 5; i++)); do
   else
     gcloud projects create "$project_id" --name="$project_name"
   fi
+
   sleep 2
   gcloud alpha billing projects link "$project_id" --billing-account="$billing_account_id"
   gcloud config set project "$project_id"
   echo -e "${ORANGE}Đã tạo dự án '$project_name' (ID: $project_id).${NC}"
 done
+
